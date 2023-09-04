bab8cdcb236b4c904de78ef9ed5ea0b89b60d6e8
Anaks-CT <anaksthayyil30@gmail.com>
2023-09-04 18:40:19 +0530

    finished designing the new Note page form. installed react-select for styled select

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import { BrowserRouter, Navigate } from "react-router-dom";
 import { Route, Routes } from "react-router";
 import { Toaster } from "react-hot-toast";
+import { NewNote } from "./pages/NewNote";
+import { backGroundImage1, backGroundImage2, backGroundImage3 } from "./assets";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/new" element={<h1>New Note</h1>} />
-        <Route path="/:id">
-          <Route index element={<h1>Show</h1>} />
-          <Route path="edit" element={<h1>Edit</h1>} />
-        </Route>
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-      <Toaster />
-    </BrowserRouter>
+    <div style={{backgroundImage: `url("${backGroundImage3}")`}} className="h-screen w-full flex flex-col justify-center bg-no-repeat bg-cover bg-center bg-fixed">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/new" element={<NewNote />} />
+          <Route path="/:id">
+            <Route index element={<h1>Show</h1>} />
+            <Route path="edit" element={<h1>Edit</h1>} />
+          </Route>
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+        <Toaster />
+      </BrowserRouter>
+    </div>
   );
 }
 
