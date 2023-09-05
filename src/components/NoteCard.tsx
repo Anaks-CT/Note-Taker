6faf93a08c570ee5f1c726a27d67cca8af6ee59e
Anaks-CT <anaksthayyil30@gmail.com>
2023-09-05 10:20:02 +0530

    finished search and filter in the home page in the last commit

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -1,33 +1,35 @@
-import { Link } from "react-router-dom"
-import { ITag } from "../interface/ITag"
+import { Link } from "react-router-dom";
+import { ITag } from "../interface/ITag";
 
 type SimplifiedNote = {
-    tags: ITag[]
-    title: string
-    id: string
-}
+  tags: ITag[];
+  title: string;
+  id: string;
+};
 
-export const NoteCard = ({id, tags, title}: SimplifiedNote) => {
-    console.log(tags)
+export const NoteCard = ({ id, tags, title }: SimplifiedNote) => {
   return (
-<Link to={`/${id}`} className="block h-100 text-reset text-decoration-none transition-transform bg-white p-4 shadow-md rounded-lg duration-100 hover:translate-y-[-5px] hover:shadow-md">
-<div className="p-4">
-  <div className="flex flex-col h-100 items-center justify-center">
-    <span className="text-lg">{title}</span>
-    {tags && tags.length > 0 && (
-      <div className="flex flex-wrap justify-center items-center mt-2">
-        {tags.map((tag) => (
-          <span
-            key={tag.id}
-            className="text-sm bg-blue-200 text-blue-800 py-1 px-2 rounded-full m-1"
-          >
-            {tag.label}
-          </span>
-        ))}
+    <Link
+      to={`/${id}`}
+      className="block h-100 text-reset text-decoration-none transition-transform bg-white p-4 shadow-md rounded-lg duration-100 hover:translate-y-[-5px] hover:shadow-md"
+    >
+      <div className="p-4">
+        <div className="flex flex-col h-100 items-center justify-center">
+          <span className="text-lg">{title}</span>
+          {tags && tags.length > 0 && (
+            <div className="flex flex-wrap justify-center items-center mt-2">
+              {tags.map((tag) => (
+                <span
+                  key={tag.id}
+                  className="text-sm bg-blue-200 text-blue-800 py-1 px-2 rounded-full m-1"
+                >
+                  {tag.label}
+                </span>
+              ))}
+            </div>
+          )}
+        </div>
       </div>
-    )}
-  </div>
-</div>
-</Link>
-  )
-}
+    </Link>
+  );
+};
